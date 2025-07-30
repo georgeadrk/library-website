ef8ca0b15fdaca426e72d82a13742519331d31e2
[George Kurniawan] <georgeadrielk@gmail.com>
2025-07-30 20:46:37 +0700

    Fixed the login and logout function

--- js/script.js
+++ js/script.js
@@ -21,3 +21,21 @@ function logout() {
   localStorage.removeItem('currentUser');
   window.location.href = 'index.html';
 }
+
+const user = JSON.parse(localStorage.getItem("bookoria_logged_in"));
+
+const guestLinks = document.getElementById("auth-guest");
+const userSection = document.getElementById("auth-user");
+const userGreeting = document.getElementById("user-greeting");
+const logoutBtn = document.getElementById("logout-btn");
+
+if (user) {
+  guestLinks.style.display = "none";
+  userGreeting.textContent = `Logged in as ${user.email}`;
+  userSection.style.display = "inline";
+}
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("bookoria_logged_in");
+  window.location.reload(); // Refresh page on logout
+});
